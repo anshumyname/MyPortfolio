7016f32b5222714416fa6eac29a6e2b0ea6acb89
Anshuman Srivastava <srivastavaanshuman33@gmail.com>
2020-12-30 12:17:10 +0530

    added exp and education

--- java_script.js
+++ java_script.js
@@ -1,9 +1,65 @@
 $(function () {
     $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
+        if ( $(window).scrollTop() > 20 ) {
             $('.navbar').addClass('active');
+            document.getElementById("navlist").style.margin = "0 80% 0 0";
         } else {
             $('.navbar').removeClass('active');
+            document.getElementById("navlist").style.margin = "0 0 0 0";
         }
     });
 });
+
+
+$(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+        
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 1500,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 1500,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 1500,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 1500,
+        });
+  
+  
+  });
+  
